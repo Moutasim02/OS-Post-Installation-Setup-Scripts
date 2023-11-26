4494ec714c4c1d4ba3be60dc3330f4bd0312856d
Moutasim02 <moutasim9997@gmail.com>
2023-11-27 01:22:45 +0400

    Fix mounting unintended behaviour

--- mount_directories.sh
+++ mount_directories.sh
@@ -6,13 +6,8 @@ USERNAME="moutasim"
 
 DIRECTORIES=("Development" "Documents" "Downloads" "Pictures" "VMs" ".ssh")
 
-# Create directories in home
 for dir in "${DIRECTORIES[@]}"; do
     mkdir -p "/home/$USERNAME/$dir"
-done
-
-# Add entries to /etc/fstab
-for dir in "${DIRECTORIES[@]}"; do
     echo "UUID=$PARTITION_UUID /home/$USERNAME/$dir ext4 defaults 0 2" | sudo tee -a /etc/fstab
 done
 
