0c82c95bc442f5759978f0591d5a5ee0542747a9
Moutasim02 <moutasim9997@gmail.com>
2023-11-27 02:09:12 +0400

    Update mount directories

--- mount_directories.sh
+++ mount_directories.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 PARTITION_UUID="uuid_here"
-
 USERNAME="moutasim"
 
-DIRECTORIES=("Development" "Documents" "Downloads" "Pictures" "VMs" ".ssh")
+declare -A DIRECTORY_MAPPING=(
+    ["Development"]="/mnt/development"
+    ["Documents"]="/mnt/documents"
+    ["Downloads"]="/mnt/downloads"
+    ["Pictures"]="/mnt/pictures"
+    ["VMs"]="/mnt/vms"
+    [".ssh"]="/mnt/ssh"
+)
 
-for dir in "${DIRECTORIES[@]}"; do
-    echo "UUID=$PARTITION_UUID /home/$USERNAME/$dir ext4 defaults 0 2" | sudo tee -a /etc/fstab
+for mount_point in "${DIRECTORY_MAPPING[@]}"; do
+    sudo mkdir -p "$mount_point"
+done
+
+for dir in "${!DIRECTORY_MAPPING[@]}"; do
+    echo "UUID=$PARTITION_UUID ${DIRECTORY_MAPPING[$dir]} ext4 defaults 0 2" | sudo tee -a /etc/fstab
 done
 
 sudo mount -a
