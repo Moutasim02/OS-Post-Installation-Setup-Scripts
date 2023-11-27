0f4b989c98da58d11de1eb1d3482e976cdfdc127
Moutasim02 <moutasim9997@gmail.com>
2023-11-27 15:24:15 +0400

    Added Development folder to the test

--- dummy_test.sh
+++ dummy_test.sh
@@ -1,4 +1,4 @@
-mkdir Downloads Documents Pictures .ssh VMs
+mkdir Downloads Documents Pictures .ssh VMs Development
 
 for dir in Downloads Documents Pictures .ssh VMs Development; do
   touch "$dir/test.txt"
